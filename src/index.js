427662dc96806d8b8a15d576ee9476272eb30dc3
George D. Kruah <dfizzkruah@gmail.com>
2023-05-06 11:50:16 +0300

    update and fix error

--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
-import addTask from './modules/addTask.js';
-import editTask from './modules/editTask.js';
-import listOfTasks from './modules/listOfTasks.js';
-import removeTask from './modules/removeTask.js';
-import TaskStore from './modules/TaskStore.js';
-import './style.scss';
+import './style.css';
+import iterateTasks from './js/iterateTasks.js';
+import eventComplete from './js/eventComplete.js';
+import draggedItem from './js/draggedItem.js';
+import addNewTaskToList from './js/addNewTask.js';
+import { buttonAction } from './js/removeTask.js';
 
-const taskStore = new TaskStore();
+iterateTasks();
 
-listOfTasks(taskStore);
+eventComplete();
 
-// ADD TASK
-document.addEventListener('keydown', (event) => {
-  const { target } = event;
-  if (target.id === 'add-new-task') {
-    if (event.key === 'Enter') {
-      addTask(taskStore, target.value);
-      target.value = '';
-    }
-  }
-});
+draggedItem();
 
-document.addEventListener('click', (event) => {
-  const { target } = event;
+buttonAction();
 
-  // EDIT TASK
-  if (target.classList.contains('task-item')) {
-    editTask(taskStore, target);
-  }
-  if (target.classList.contains('task-title')) {
-    const parent = target.parentNode.parentNode;
-    editTask(taskStore, parent);
-  }
-
-  // REMOVE TASK
-  if (target.classList.contains('delete-task')) {
-    removeTask(taskStore, target.id);
-  }
-});
+addNewTaskToList();
--- src/modules/Storage.js
+++
@@ -1,7 +0,0 @@
-const getTasks = () => JSON.parse(localStorage.getItem('tasks')) || [];
-
-const saveTasks = (tasksArray) => {
-  localStorage.setItem('tasks', JSON.stringify(tasksArray));
-};
-
-export { getTasks, saveTasks };
